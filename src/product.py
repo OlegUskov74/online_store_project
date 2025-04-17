e2bc6ebd17a1ff2ec96c1dfd3b60ee99e2f47836
Олег Усков <oleg81uskov@gmail.com>
2025-04-17 16:57:54 +0500

    fixses readme.md

--- src/product.py
+++ src/product.py
@@ -103,51 +103,51 @@ class Product(BaseProduct, PrintMixin):
 
         return product
 
-if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 190000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 35000.0, 14)
-    # product4 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 10)
-
-    print(product1.name)
-    print(product1.description)
-    print(product1.price)
-    print(product1.quantity)
-
-    print(product2.name)
-    print(product2.description)
-    print(product2.price)
-    print(product2.quantity)
-
-    print(product3.name)
-    print(product3.description)
-    print(product3.price)
-    print(product3.quantity)
-
-    product_list = [{
-        "name": "Samsung Galaxy S23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 190000.0,
-        "quantity": 5,},
-                    {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
-                    {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 35000.0, "quantity": 14}]
-
-
-    new_product =Product.new_product({
-        "name": "Samsung Galaxy S23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 170000.0,
-        "quantity": 7},
-        product_list)
-
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    print("-------------------")
-
-    print(product1 + product2)
-    print(product1 + product3)
-    print(product2 + product3)
-
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 190000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 35000.0, 14)
+#     # product4 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 10)
+#
+#     print(product1.name)
+#     print(product1.description)
+#     print(product1.price)
+#     print(product1.quantity)
+#
+#     print(product2.name)
+#     print(product2.description)
+#     print(product2.price)
+#     print(product2.quantity)
+#
+#     print(product3.name)
+#     print(product3.description)
+#     print(product3.price)
+#     print(product3.quantity)
+#
+#     product_list = [{
+#         "name": "Samsung Galaxy S23 Ultra",
+#         "description": "256GB, Серый цвет, 200MP камера",
+#         "price": 190000.0,
+#         "quantity": 5,},
+#                     {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+#                     {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 35000.0, "quantity": 14}]
+#
+#
+#     new_product =Product.new_product({
+#         "name": "Samsung Galaxy S23 Ultra",
+#         "description": "256GB, Серый цвет, 200MP камера",
+#         "price": 170000.0,
+#         "quantity": 7},
+#         product_list)
+#
+#     print(new_product.name)
+#     print(new_product.description)
+#     print(new_product.price)
+#     print(new_product.quantity)
+#
+#     print("-------------------")
+#
+#     print(product1 + product2)
+#     print(product1 + product3)
+#     print(product2 + product3)
+#
